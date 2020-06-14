3d8b66ab9de4d93b5b922d79ea36a03651f09fb8
Miguel Rosa <miguelrosa.programacao@gmail.com>
2020-06-14 12:23:57 +0100

    update CSS

--- src/pages/sitestruct/navigationbar/NavigationBar.js
+++ src/pages/sitestruct/navigationbar/NavigationBar.js
@@ -7,19 +7,21 @@ class NavigationBar extends Component {
 
     render() {
         return(
-            <nav className="navbar navbar-expand-md navbar-dark">
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#mainNavBar" aria-controls="mainNavBar" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"/>
-                </button>
-                <div className="collapse navbar-collapse" id="mainNavBar">
-                    <div className="navbar-nav">
-                        <NavLink className="nav-item nav-link" exact to="/">Home</NavLink>
-                        <NavLink className="nav-item nav-link" to="/novidades">Novidades</NavLink>
-                        <NavLink className="nav-item nav-link" to="/filmes">Filmes</NavLink>
-                        <NavLink className="nav-item nav-link" to="/">Favoritos</NavLink>
+            <div className="mainNavBar">
+                <nav className="navbar navbar-expand-md navbar-dark">
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#mainNavBar" aria-controls="mainNavBar" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"/>
+                    </button>
+                    <div className="collapse navbar-collapse">
+                        <div className="navbar-nav">
+                            <NavLink className="nav-item nav-link" exact to="/">Home</NavLink>
+                            <NavLink className="nav-item nav-link" to="/novidades">Novidades</NavLink>
+                            <NavLink className="nav-item nav-link" to="/filmes">Filmes</NavLink>
+                            <NavLink className="nav-item nav-link" to="/">Favoritos</NavLink>
+                        </div>
                     </div>
-                </div>
-            </nav>
+                </nav>
+            </div>
         );
     }
 
