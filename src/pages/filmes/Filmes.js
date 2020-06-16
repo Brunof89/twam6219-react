9c567ea46e43f8e271afcf70bcafb015b1060665
Brunof89 <brunof.89@gmail.com>
2020-06-16 10:50:35 +0100

    Get Films by Category

--- src/pages/filmes/Filmes.js
+++ src/pages/filmes/Filmes.js
@@ -2,7 +2,8 @@ import React, { Component } from "react";
 import Axios from "axios";
 
 import { API_BASE_URL, API_KEY, LANGUAGE } from "../../services/const";
-import './filmes.css'
+import './filmes.css';
+import CoversRow from '../novidades/CoversRow';
 
 export default class Filmes extends Component {
 
@@ -11,11 +12,9 @@ export default class Filmes extends Component {
 
         this.state = {
             genres: [],
-
-        };
-
-        //this.getCategoriesFromAPI();
-        //this.getOptionSelected();
+			films:[]	
+        };		
+		this.getOptionSelected = this.getOptionSelected.bind(this);
     }
 
     componentDidMount() {
@@ -23,14 +22,31 @@ export default class Filmes extends Component {
 
         Axios.get(categoriesApi)
             .then(({ data }) => {
+				
                 const { genres } = data;
 
                 this.setState({ genres });
         });
     }
 
-    getOptionSelected = () => {
-        console.log('option: ');
+    getOptionSelected(e,a,b) {
+		let categoriesApi = API_BASE_URL + '/search/multi?api_key=' +API_KEY +'&query='+ e.target.value + '&language=' + LANGUAGE + '&page=1';
+
+		Axios.get(categoriesApi)
+            .then(({ data }) => {
+				 const results = data.results;
+
+                    var coverRows = [];
+
+                    results.forEach( (movie) => {
+                        movie.poster_src = 'https://image.tmdb.org/t/p/w300' + movie.poster_path;
+                        const movieRow = <CoversRow key={movie.id} movie={movie} />
+                        coverRows.push(movieRow);
+                    });
+
+                    this.setState({ films: coverRows }); //element (rows) for the "render"
+
+        });
     }
 
     render() {
@@ -40,14 +56,16 @@ export default class Filmes extends Component {
             <div>
                 <h2>Página Novidades - FAZER CSS!!</h2>
                 <p>Seleccione uma categoria</p>
-                <select className="form-control mt-4 col-md-2 col-offset-4" id="categorySelectBox">
+                <select className="form-control mt-4 col-md-2 col-offset-4" id="categorySelectBox" onChange={this.getOptionSelected}>
                     {genres.map( ({ id, name }) => (
                         <option key={id} id={id} value={name}> {name} </option>
                     ))}
                 </select>
-                <button className="btn btn-success btnSearchCategory" onClick={""}>Pesquisar</button>
+                
                 <hr/>
-                <p>dfdfdf</p>
+				<div>                
+                {this.state.films}
+				</div>
             </div>
         );
     }
